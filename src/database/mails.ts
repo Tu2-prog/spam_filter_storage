20617ffd588181f0a94e90979d3b59d464ec1cda
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-19 23:09:25 +0100

    static code check - fixing unused imports and used number instead of Number

--- src/database/mails.ts
+++ src/database/mails.ts
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const MailSchema = new mongoose.Schema({
     content: {type: String, required: true},
@@ -8,6 +8,6 @@ const MailSchema = new mongoose.Schema({
 export const MailModel = mongoose.model('Mail', MailSchema);
 
 export const getMail =  () => MailModel.find();
-export const createMailEntry = (values: Record<string, Number>) => new MailModel(values).save().then((mail) => mail.toObject());
+export const createMailEntry = (values: Record<string, number>) => new MailModel(values).save().then((mail) => mail.toObject());
 export const deleteMailByID = (id: string) => MailModel.findOneAndDelete({_id: id});
 export const updateMailByID = (id: string, values: Record<string, number>) => MailModel.findByIdAndUpdate(id, values);
