6e773373215a58f0338f329503db1b2916bb8c63
Tu2-prog <anhtuduong.nguyen@outlook.de>
2023-12-28 14:45:31 +0100

    removed unneccesary function part 2

--- src/router/storage.ts
+++ src/router/storage.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 
-import { create, deleteAllMails, deleteMail, getMails } from '../adapter/controller';
+import { create, deleteAllMails, getMails } from '../adapter/controller';
 
 export default (router: express.Router) => {
     router.post('/store/create', create);
-    router.delete('/store/delete/:id', deleteMail);
     router.delete('/store/deleteall', deleteAllMails);
     router.get('/store', getMails)
 }
